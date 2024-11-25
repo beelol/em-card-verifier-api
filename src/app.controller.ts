ffa196e16b55b367f07b1e04c692dcf076bb0824
Bilal Itani <bilalitani1@gmail.com>
2024-11-25 17:37:24 -0600

    Add validation for credit card using DTO. Reject missing, non-string, and alphanumeric input as invalid

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,15 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { VerifyCreditCardResponse } from './types';
+import { VerifyCreditCardDto } from './validation/creditcard.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post('/verify')
-  verifyCreditCard(
-    @Body('candidate') candidate: string,
-  ): VerifyCreditCardResponse {
-    if (!candidate) {
-      throw new BadRequestException('Credit card number is required');
-    }
-
-    const cardIsValid = this.appService.verifyCreditCard(candidate);
+  verifyCreditCard(@Body() dto: VerifyCreditCardDto): VerifyCreditCardResponse {
+    const cardIsValid = this.appService.verifyCreditCard(dto.candidate);
 
     return { cardIsValid };
   }
